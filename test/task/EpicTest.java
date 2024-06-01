4519f29d0ed0cfae2f4462a5a334eb05a6922c4c
Sylaman <Sylaman.snake@gmail.com>
2024-06-01 12:02:52 +0300

    add JUnit and create test packages

---
+++ test/task/EpicTest.java
@@ -0,0 +1,16 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpicTest {
+
+    @Test
+    void addSubtask() {
+    }
+
+    @Test
+    void clearSubtasks() {
+    }
+}
---
+++ test/task/SubtaskTest.java
@@ -0,0 +1,16 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubtaskTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getEpicID() {
+    }
+}
